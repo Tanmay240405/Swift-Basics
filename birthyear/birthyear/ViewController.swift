dc7bd17a74e22471b803363cafb6f6d35add3ef4
SDC-USER <sdc-user@VY-7F-IOSTC-084.local>
2025-07-29 15:40:55 +0530

    birthyear

--- birthyear/birthyear/ViewController.swift
+++ birthyear/birthyear/ViewController.swift
@@ -6,6 +6,14 @@
 //
 
 import UIKit
+func dateTimeFormat(from date: Date) -> (Int) {
+    let calender = Calendar.current
+    let components = calender.dateComponents([.year], from: date)
+    return (
+        components.year ?? 0
+       
+    )
+}
 
 class ViewController: UIViewController {
 
@@ -19,12 +27,14 @@ class ViewController: UIViewController {
 
     
     @IBAction func calcButton(_ sender: Any) {
+        let date = Date()
+        let year = dateTimeFormat(from: date)
         let num = Int(enterYear.text ?? "") ?? 0
-        if num == 0 || num > 2025{
+        if num == 0 || num > year{
             printLabel.text = "Invalid"
         }
         else{
-            let age = 2025 - num
+            let age = year - num
             printLabel.text = String(age)
         }
     }
---
+++ method/method/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  method
+//
+//  Created by SDC-USER on 29/07/25.
+//  Functions(dividing code into modules (modularity))
+// to make a code more readable and more functional
+//  function without arguments or return value
+//  with arg without ret val
+//  with arg and ret val
+//  without arg with ret val
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
