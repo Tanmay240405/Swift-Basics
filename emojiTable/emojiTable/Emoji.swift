9872add899e7e8cbe39097c6fc74d885da353bb4
SDC-USER <tanmaysaxena2404@gmail.com>
2025-10-06 16:43:12 +0530

    table

---
+++ emojiTable/emojiTable/Emoji.swift
@@ -0,0 +1,24 @@
+//
+//  Emoji.swift
+//  emojiTable
+//
+//  Created by SDC-USER on 06/10/25.
+//
+
+import Foundation
+
+
+
+struct Emojis {
+    var symbol: String
+    var name: String
+    var descriptiomn: String
+    var usage: String
+}
+
+var emojis: [Emojis] = [
+    Emojis(symbol: "😋", name: "Tasty", descriptiomn: "very tasy face", usage: "to show it is tasty"),
+    Emojis(symbol: "❤️", name: "heart", descriptiomn: "heart", usage: "to show affetcion"),
+    Emojis(symbol: "😭", name: "cry", descriptiomn: "crying face", usage: "to show person is sad"),
+    Emojis(symbol: "😋", name: "Tasty", descriptiomn: "very tasy face", usage: "to show it is tasty")
+]
