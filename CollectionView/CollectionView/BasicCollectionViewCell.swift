15863f59238d37b242205351e842198ce4540914
SDC-USER <tanmaysaxena2404@gmail.com>
2025-11-18 11:08:55 +0530

    a

---
+++ CollectionView/CollectionView/BasicCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  BasicCollectionViewCell.swift
+//  CollectionView
+//
+//  Created by SDC-USER on 18/11/25.
+//
+
+import UIKit
+
+class BasicCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    
+}
